ea1ebba45172c5cf7b4c7d6ef15569723caa2835
kfatman023 <kamvelihlefatman@gmail.com>
2024-11-15 11:59:23 +0200

    added hero-banner

--- src/components/hero-banner.jsx
+++ src/components/hero-banner.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 
-type HeroBannerProps = {
-  onBetNow: () => void;
-};
-
-const HeroBanner: React.FC<HeroBannerProps> = ({ onBetNow }) => {
+const HeroBanner = ({ onBetNow }) => {
   return (
     <section className="flex flex-col items-start mt-72 ml-20 max-w-full text-3xl w-[803px] max-md:mt-10">
       <h2 className="self-stretch text-6xl max-md:max-w-full max-md:text-4xl">
