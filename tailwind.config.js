ef43ef1a5afec5fda5b43e6f36eb6eb7d2a46aac
kfatman023 <kamvelihlefatman@gmail.com>
2024-11-15 12:20:00 +0200

    removed layouts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Ensure this matches your project structure
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
