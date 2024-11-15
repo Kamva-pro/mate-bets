1482b79077a47bab35633521f6910adcf9376330
kfatman023 <kamvelihlefatman@gmail.com>
2024-11-15 13:17:30 +0200

    added filter item

--- src/components/FilterItem.jsx
+++ src/components/FilterItem.jsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React from 'react';
+import styles from './HeroSection.module.css';
 
-function FilterItem({ label, imageSrc }) {
+const FilterItem = ({ text, iconSrc }) => {
   return (
-    <div className="flex overflow-hidden gap-10 items-start pt-3 pr-2 pb-5 pl-9 rounded border border-white border-solid max-md:pl-5">
-      <div className="my-auto">{label}</div>
-      <img loading="lazy" src={imageSrc} alt="" className="object-contain shrink-0 self-start aspect-square w-[50px]" />
+    <div className={styles.filterItem}>
+      <div className={styles.filterText}>{text}</div>
+      <img loading="lazy" src={iconSrc} alt="" className={styles.filterIcon} />
     </div>
   );
-}
+};
 
 export default FilterItem;
---
+++ src/components/NavItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './HeroSection.module.css';
+
+const NavItem = ({ text }) => {
+  return (
+    <div className={styles.navItem}>
+      {text}
+    </div>
+  );
+};
+
+export default NavItem;
