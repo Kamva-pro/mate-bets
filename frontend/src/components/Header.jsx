1428832808977948ca101465c83773c391e4a7d0
Kamva-pro <kamvelihlefatman@gmail.com>
2024-11-19 17:35:56 +0200

    styling

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,7 +4,7 @@ import '../css/Navbar.css';
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="navbar-brand">My Brand</div>
+      <div className="navbar-brand">Mate<span id='bets-text'> Bets</span></div>
       <div className="navbar-links">
         <a href="#home" className="navbar-link">Home</a>
         <a href="#about" className="navbar-link">About</a>
