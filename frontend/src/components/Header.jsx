13bfbca2e4606c0bb0b309c36e77b65017c8ace1
Kamva-pro <kamvelihlefatman@gmail.com>
2024-11-19 17:31:57 +0200

    added css

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,50 +1,17 @@
-import React, { useState } from 'react';
-import { Layout, Menu, Button, Drawer } from 'antd';
-import { MenuOutlined } from '@ant-design/icons';
-
-const { Header } = Layout;
+import React from 'react';
+import '../css/Navbar.css';
 
 function Navbar() {
-  const [drawerVisible, setDrawerVisible] = useState(false);
-
-  const showDrawer = () => setDrawerVisible(true);
-  const closeDrawer = () => setDrawerVisible(false);
-
   return (
-    <Layout>
-      <Header style={{ position: 'sticky', top: 0, zIndex: 1, width: '100%' }}>
-        <div className="logo" style={{ color: 'white', fontSize: '20px', fontWeight: 'bold' }}>My App</div>
-
-        {/* Hamburger Menu for Mobile/Tablet */}
-        <Button 
-          className="mobile-menu" 
-          icon={<MenuOutlined />} 
-          onClick={showDrawer}
-          style={{ display: 'block', marginLeft: 'auto', background: 'transparent', border: 'none', color: 'white' }}
-        />
-
-        {/* Mobile Drawer */}
-        <Drawer
-          title="Menu"
-          placement="right"
-          onClose={closeDrawer}
-          visible={drawerVisible}
-        >
-          <Menu mode="vertical">
-            <Menu.Item key="1"><a href="#home">Home</a></Menu.Item>
-            <Menu.Item key="2"><a href="#about">About</a></Menu.Item>
-            <Menu.Item key="3"><a href="#contact">Contact</a></Menu.Item>
-          </Menu>
-        </Drawer>
-
-        {/* Desktop Menu */}
-        <Menu theme="dark" mode="horizontal" style={{ display: 'inline-block', width: 'auto' }}>
-          <Menu.Item key="1"><a href="#home">Home</a></Menu.Item>
-          <Menu.Item key="2"><a href="#about">About</a></Menu.Item>
-          <Menu.Item key="3"><a href="#contact">Contact</a></Menu.Item>
-        </Menu>
-      </Header>
-    </Layout>
+    <nav className="navbar">
+      <div className="navbar-brand">My Brand</div>
+      <div className="navbar-links">
+        <a href="#home" className="navbar-link">Home</a>
+        <a href="#about" className="navbar-link">About</a>
+        <a href="#contact" className="navbar-link">Contact</a>
+      </div>
+      <button className="navbar-button">Sign Up</button>
+    </nav>
   );
 }
 
