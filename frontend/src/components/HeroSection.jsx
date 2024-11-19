ee29d3f4294c82c758c6befce7a3f65011914e29
Kamva-pro <kamvelihlefatman@gmail.com>
2024-11-19 16:51:57 +0200

    added bootstrap

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -12,18 +12,3 @@ export function HeroLayout() {
   );
 }
 
-const StyledHeroSection = styled.section`
-  background-color: #121212;
-  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
-  display: flex;
-  flex-direction: column;
-  align-items: start;
-  font-family: Montserrat, sans-serif;
-  color: #fff;
-  font-weight: 700;
-  padding: 27px 31px 110px;
-
-  @media (max-width: 991px) {
-    padding: 0 20px 100px;
-  }
-`;
