f13f7ca8428e890904398f51a55cd7481aeca4ca
Kamva-pro <kamvelihlefatman@gmail.com>
2024-11-20 06:05:43 +0200

    navlinks styling

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,7 +6,7 @@ function Navbar() {
     <nav className="navbar">
       <div className="navbar-brand">Mate<span id='bets-text'> Bets</span></div>
       <div className="navbar-links">
-        <a href="#home" className="navbar-link">Home</a>
+        <a href="#home"  className="navbar-link active">Home</a>
         <a href="#about" className="navbar-link">Play a Friend</a>
         <a href="#contact" className="navbar-link">Pro Games</a>
       </div>
