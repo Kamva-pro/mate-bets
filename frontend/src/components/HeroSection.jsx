690812de82f577be7c64cd1506cfc33875e88b4c
Kamva-pro <kamvelihlefatman@gmail.com>
2024-11-21 04:35:43 +0200

    hero css

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -8,6 +8,7 @@ import "../css/Hero.css";
         <div className='hero-content'>
         <h1>Put Your Money Where Your Moves Are!</h1>
         <h6>Play against your friends and win big!</h6>
+        <button className='bet-now'>Bet Now</button>
       </div></div>
       
     </div>
