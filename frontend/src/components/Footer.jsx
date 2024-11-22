ce28111096918625a5cfe97a0eec2ec3f20f59a6
Kamva-pro <kamvelihlefatman@gmail.com>
2024-11-23 01:01:17 +0200

    footer grid

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
-const Footer = () => {}
+const Footer = () => {
+    return(
+        <div class="grid-container">
+  <div class="grid-item first">Column 1</div>
+  <div class="grid-item">Column 2</div>
+  <div class="grid-item">Column 3</div>
+</div>
+
+    )
+}
 
 export default Footer
