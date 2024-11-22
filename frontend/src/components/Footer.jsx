c20e6fe9517979e3fdbd2bf976bebf18d02178fb
Kamva-pro <kamvelihlefatman@gmail.com>
2024-11-23 01:39:30 +0200

    footer summary styling

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,10 +5,13 @@ const Footer = () => {
     return(
         <div class="footer-container">
             <div class="grid-item first">
-            <div className="navbar-brand">Mate<span id='bets-text'> Bets</span></div>
+                <div className="footer-summary">
+                <div className="navbar-brand">Mate<span id='bets-text'> Bets</span></div>
             <p>Mate Bets is a chess betting website where you can bet against your 
                 friends on your own personal games and even pro games. 
             </p>
+                </div>
+            
             </div>
             <div class="grid-item">Column 2</div>
             <div class="grid-item">Column 3</div>
