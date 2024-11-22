8e1747e3d7ca0d1600ab2d25db029f7e5e03a06c
Kamva-pro <kamvelihlefatman@gmail.com>
2024-11-22 19:48:00 +0200

    procard component

---
+++ frontend/src/components/ProCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../css/ProGames.css";
+
+const ProCard = () => {
+    <div className="progames-card">
+    <div className='player-info'>
+    <img className='profile-thumbnail' src="../src/assets/kd.jpg" />
+    <p>Player one</p>
+    </div>
+    <h3>VS</h3>
+    <div className='player-info'>
+    <img className='profile-thumbnail' src="../src/assets/conor.jpg" />
+    <p>Player two</p>
+    </div>            
+    
+</div>
+}
+
+export default ProCard
--- frontend/src/components/ProGames.jsx
+++ frontend/src/components/ProGames.jsx
@@ -1,23 +1,12 @@
 import React from 'react';
 import "../css/ProGames.css";
+import ProCard from './ProCard';
 
  function ProGames()
  {
     return(
         <div className="progames-section">
-        <div className="progames-card">
-            <div className='player-info'>
-            <img className='profile-thumbnail' src="../src/assets/kd.jpg" />
-            <p>Player one</p>
-            </div>
-            <h3>VS</h3>
-            <div className='player-info'>
-            <img className='profile-thumbnail' src="../src/assets/conor.jpg" />
-            <p>Player two</p>
-            </div>            
-            
-        </div>
-        <div className="progames-card">Card 2</div>
+            <ProCard/>
         </div>
     )
  }
