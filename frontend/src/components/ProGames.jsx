5ea3238d044dd3fd2f5b8753697d1f217cd6d16c
Kamva-pro <kamvelihlefatman@gmail.com>
2024-11-22 19:54:54 +0200

    used props

--- frontend/src/components/ProGames.jsx
+++ frontend/src/components/ProGames.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React from "react";
 import "../css/ProGames.css";
-import ProCard from './ProCard';
+import ProCard from "./ProCard";
 
- function ProGames()
- {
-    return(
+function ProGames() {
+    return (
         <div className="progames-section">
-            <ProCard/>
-            <ProCard/>
+            {/* Dynamically specify player information */}
+            <ProCard 
+                playerOne="Player One" 
+                playerOneImg="../src/assets/kd.jpg" 
+                playerTwo="Player Two" 
+                playerTwoImg="../src/assets/conor.jpg" 
+            />
+            <ProCard 
+                playerOne="Alice" 
+                playerOneImg="../src/assets/alice.jpg" 
+                playerTwo="Bob" 
+                playerTwoImg="../src/assets/bob.jpg" 
+            />
         </div>
-    )
- }
+    );
+}
 
- export default ProGames
+export default ProGames;
