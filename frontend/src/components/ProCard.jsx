4ee134770b592d3267ddbd8293dbfda6a4a19c8d
Kamva-pro <kamvelihlefatman@gmail.com>
2024-11-22 19:52:26 +0200

    added return statement to procard

--- frontend/src/components/ProCard.jsx
+++ frontend/src/components/ProCard.jsx
@@ -2,18 +2,19 @@ import React from "react";
 import "../css/ProGames.css";
 
 const ProCard = () => {
-    <div className="progames-card">
-    <div className='player-info'>
-    <img className='profile-thumbnail' src="../src/assets/kd.jpg" />
-    <p>Player one</p>
-    </div>
-    <h3>VS</h3>
-    <div className='player-info'>
-    <img className='profile-thumbnail' src="../src/assets/conor.jpg" />
-    <p>Player two</p>
-    </div>            
-    
-</div>
-}
+    return ( 
+        <div className="progames-card">
+            <div className='player-info'>
+                <img className='profile-thumbnail' src="../src/assets/kd.jpg" alt="Player one" />
+                <p>Player one</p>
+            </div>
+            <h3>VS</h3>
+            <div className='player-info'>
+                <img className='profile-thumbnail' src="../src/assets/conor.jpg" alt="Player two" />
+                <p>Player two</p>
+            </div>            
+        </div>
+    );
+};
 
-export default ProCard
+export default ProCard;
