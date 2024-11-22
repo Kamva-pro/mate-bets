6b38ad52dd87ccae16238e926c7fb2d19f933ac2
Kamva-pro <kamvelihlefatman@gmail.com>
2024-11-22 19:31:45 +0200

    player profile

--- frontend/src/components/ProGames.jsx
+++ frontend/src/components/ProGames.jsx
@@ -5,7 +5,18 @@ import "../css/ProGames.css";
  {
     return(
         <div className="progames-section">
-        <div className="progames-card">Card 1</div>
+        <div className="progames-card">
+            <div className='player-info'>
+            <img className='profile-thumbnail' src="../src/assets/kd.jpg" />
+            <p>Player one</p>
+            </div>
+            <h3>VS</h3>
+            <div className='player-info'>
+            <img className='profile-thumbnail' src="../src/assets/conor.jpg" />
+            <p>Player two</p>
+            </div>            
+            
+        </div>
         <div className="progames-card">Card 2</div>
         </div>
     )
