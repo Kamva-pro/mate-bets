c3c8db635dc57248a2e7315b58a08357c124831c
Kamva-pro <kamvelihlefatman@gmail.com>
2024-11-23 00:38:08 +0200

    updated navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,31 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../css/Responsive.css";
 import '../css/Navbar.css';
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-brand">Mate<span id='bets-text'> Bets</span></div>
+
+      {/* Desktop Navbar Links */}
       <div className="navbar-links">
-        <a href="#home"  className="navbar-link active">Home</a>
+        <a href="#home" className="navbar-link active">Home</a>
         <a href="#about" className="navbar-link">Play a Friend</a>
         <a href="#contact" className="navbar-link">Pro Games</a>
       </div>
+
       <button className="navbar-button">Login</button>
-      <h3 id='menu' >Menu</h3>
+
+      {/* Menu Icon for Mobile */}
+      <img
+        id="menu"
+        src="../src/assets/menu.png"
+        alt="Menu Icon"
+        className="menu-icon"
+        onClick={toggleMenu}
+      />
+
+      {/* Mobile Menu Links */}
+      {isMenuOpen && (
+        <div className="menu-links">
+          <a href="#home" className="navbar-link active">Home</a>
+          <a href="#about" className="navbar-link">Play a Friend</a>
+          <a href="#contact" className="navbar-link">Pro Games</a>
+        </div>
+      )}
     </nav>
   );
-}
-
-// const showMenu = () => {
-//   return(
-//     <div className="menu-links">
-//         <a href="#home"  className="navbar-link active">Home</a>
-//         <a href="#about" className="navbar-link">Play a Friend</a>
-//         <a href="#contact" className="navbar-link">Pro Games</a>
-//       </div>
-      
-//   )
-// }
+};
 
 export default Navbar;
