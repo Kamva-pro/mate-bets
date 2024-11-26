9964ef4bf4f6ceda7392f0e02918245626098c83
Kamva-pro <kamvelihlefatman@gmail.com>
2024-11-26 21:14:28 +0200

    progames text

--- frontend/src/components/ProGamesPage.jsx
+++ frontend/src/components/ProGamesPage.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import Navbar from "./Header";
+import ProGames from "./ProGames";
+import FilterGames from "./Filter";
+import "../css/ProGames.css";
 
 const ProGamesPage = () => {
     return(
-        <Navbar/>
+        <div id="progamespage"> 
+            <Navbar/>
+            <h4>Bet on Professional Chess Games</h4>
+            <FilterGames/>
+            <ProGames/>
+        </div>
     )
 }
 
