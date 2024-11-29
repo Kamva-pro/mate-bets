398d1c4d4230f7cde061d5b9d2ae3c3bbe3ccc5b
Kamva-pro <kamvelihlefatman@gmail.com>
2024-11-29 13:15:41 +0200

    fixed betform

--- frontend/src/components/BetForm.jsx
+++ frontend/src/components/BetForm.jsx
@@ -27,11 +27,14 @@ const Card = styled(MuiCard)(({ theme }) => ({
   flexDirection: 'column',
   alignSelf: 'center',
   width: '100%',
-  padding: theme.spacing(4),
-  gap: theme.spacing(2),
+  padding: theme.spacing(3),
+  gap: theme.spacing(1.5),
   margin: 'auto',
+  maxWidth: '100%',
   [theme.breakpoints.up('sm')]: {
     maxWidth: '450px',
+    padding: theme.spacing(4),
+    gap: theme.spacing(2),
   },
   boxShadow:
     'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
@@ -41,10 +44,10 @@ const Card = styled(MuiCard)(({ theme }) => ({
   }),
 }));
 
-
 const BetContainer = styled(Stack)(({ theme }) => ({
   height: 'calc((1 - var(--template-frame-height, 0)) * 100dvh)',
-  minHeight: '100%',
+  height: '100%',
+  marginTop: "100px",
   padding: theme.spacing(2),
   [theme.breakpoints.up('sm')]: {
     padding: theme.spacing(4),
@@ -55,13 +58,13 @@ const BetContainer = styled(Stack)(({ theme }) => ({
     position: 'absolute',
     zIndex: -1,
     inset: 0,
-    backgroundImage:
-      'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
-    backgroundRepeat: 'no-repeat',
-    ...theme.applyStyles('dark', {
-      backgroundImage:
-        'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))',
-    }),
+    // backgroundImage:
+    //   'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
+    // backgroundRepeat: 'no-repeat',
+    // ...theme.applyStyles('dark', {
+    //   backgroundImage:
+    //     'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))',
+    // }),
   },
 }));
 
@@ -158,117 +161,102 @@ export default function BetForm(props) {
 
   
 
-  return (
-    <AppTheme {...props}>
-            <CssBaseline enableColorScheme />
-      <BetContainer>
-      <ColorModeSelect sx={{ position: 'fixed', top: '1rem', right: '1rem' }} />
-      {alertMessage && (
-        <Alert variant="outlined" severity={alertSeverity} style={{ marginBottom: '20px' }}>
-          {alertMessage}
-        </Alert>
-      )}
-        <Card variant="outlined">
-          <Typography
-            component="h1"
-            variant="h4"
-            sx={{ fontSize: 'clamp(1.5rem, 5vw, 2rem)', textAlign: 'start' }}
-          >
-            Place Your Bet!
-          </Typography>
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{
-              display: 'grid',
-              gridTemplateColumns: '1fr',
-              gap: 2,
-            }}
-          >
-            {/* Opponent Username */}
-            <FormControl>
-              <FormLabel htmlFor="opponentEmail">Opponent's Email</FormLabel>
-              <TextField
-                id="opponentEmail"
-                type="email"
-                name="opponentEmail"
-                value={opponentEmail}
-                onChange={(e) => setOpponentEmail(e.target.value)}
-                placeholder="Enter opponent's email"
-                required
-                fullWidth
-                variant="outlined"
-                color={'primary'}
-              />
-            </FormControl>
-
-            {/* Game Format */}
-            <FormControl>
-              <FormLabel htmlFor="gameFormat">Game Format</FormLabel>
-              <TextField
-                id="gameFormat"
-                select
-                value={gameFormat}
-                onChange={(e) => setGameFormat(e.target.value)}
-                SelectProps={{ native: true }}
-                required
-                fullWidth
-                variant="outlined"
-              >
-                <option value="" disabled>Select a format</option>
-                <option value="blitz">Blitz</option>
-                <option value="rapid">Rapid</option>
-                <option value="bullet">Bullet</option>
-              </TextField>
-            </FormControl>
-
-            {/* Match Type */}
-            <FormControl>
-              <FormLabel htmlFor="gameseries">Game Series</FormLabel>
-              <TextField
-                id="gameseries"
-                select
-                value={gameSeries}
-                onChange={(e) => setGameSeries(e.target.value)}
-                SelectProps={{ native: true }}
-                required
-                fullWidth
-                variant="outlined"
-              >
-                <option value="" disabled>Game Series</option>
-                <option value="one_game">One Game</option>
-                <option value="best_of_three">Best of Three</option>
-                <option value="best_of_five">Best of Five</option>
-
-              </TextField>
-            </FormControl>
-
-            {/* Chess Website */}
-            {/* <FormControl>
-              <FormLabel htmlFor="chessWebsite">Chess Website</FormLabel>
-              <TextField
-                id="chessWebsite"
-                select
-                value={chessWebsite}
-                onChange={(e) => setChessWebsite(e.target.value)}
-                SelectProps={{ native: true }}
-                required
-                fullWidth
-                variant="outlined"
-              >
-                <option value="" disabled>Select a website</option>
-                <option value="chess.com">Chess.com</option>
-                <option value="lichess.org">Lichess.org</option>
-              </TextField>
-            </FormControl> */}
-
-            {/* Chess Username */}
-            
+    return (
+      <AppTheme {...props}>
+        <CssBaseline enableColorScheme />
+        <BetContainer>
+          <ColorModeSelect sx={{ position: 'fixed', top: '1rem', right: '1rem' }} />
+          {alertMessage && (
+            <Alert variant="outlined" severity={alertSeverity} style={{ marginBottom: '20px' }}>
+              {alertMessage}
+            </Alert>
+          )}
+          <Card variant="outlined">
+            <Typography
+              component="h1"
+              variant="h4"
+              sx={{ fontSize: 'clamp(1.5rem, 5vw, 2rem)', textAlign: 'start' }}
+            >
+              Place Your Bet!
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              noValidate
+              sx={{
+                display: 'grid',
+                gridTemplateColumns: '1fr',
+                gap: 2,
+                maxHeight: '80vh',  // Adjust the height as needed
+                overflowY: 'auto',  // Adds the scrollbar
+                paddingRight: '10px',  // Adjust for scrollbar visibility if needed
+                '&::-webkit-scrollbar': {
+                  display: 'none',  // Hides scrollbar for Webkit browsers
+                },
+                '-ms-overflow-style': 'none',  // Hides scrollbar for IE/Edge
+                'scrollbar-width': 'none',  // Hides scrollbar for Firefox
+              }}
+            >
+              {/* Opponent Username */}
               <FormControl>
-                <FormLabel htmlFor="chessUsername">
-                  Lichess Username
-                </FormLabel>
+                <FormLabel htmlFor="opponentEmail">Opponent's Email</FormLabel>
+                <TextField
+                  id="opponentEmail"
+                  type="email"
+                  name="opponentEmail"
+                  value={opponentEmail}
+                  onChange={(e) => setOpponentEmail(e.target.value)}
+                  placeholder="Enter opponent's email"
+                  required
+                  fullWidth
+                  variant="outlined"
+                  color={'primary'}
+                />
+              </FormControl>
+    
+              {/* Game Format */}
+              <FormControl>
+                <FormLabel htmlFor="gameFormat">Game Format</FormLabel>
+                <TextField
+                  id="gameFormat"
+                  select
+                  value={gameFormat}
+                  onChange={(e) => setGameFormat(e.target.value)}
+                  SelectProps={{ native: true }}
+                  required
+                  fullWidth
+                  variant="outlined"
+                >
+                  <option value="" disabled>Select a format</option>
+                  <option value="blitz">Blitz</option>
+                  <option value="rapid">Rapid</option>
+                  <option value="bullet">Bullet</option>
+                </TextField>
+              </FormControl>
+    
+              {/* Match Type */}
+              <FormControl>
+                <FormLabel htmlFor="gameseries">Game Series</FormLabel>
+                <TextField
+                  id="gameseries"
+                  select
+                  value={gameSeries}
+                  onChange={(e) => setGameSeries(e.target.value)}
+                  SelectProps={{ native: true }}
+                  required
+                  fullWidth
+                  variant="outlined"
+                >
+                  <option value="" disabled>Game Series</option>
+                  <option value="one_game">One Game</option>
+                  <option value="best_of_three">Best of Three</option>
+                  <option value="best_of_five">Best of Five</option>
+                </TextField>
+              </FormControl>
+    
+              {/* Chess Username */}
+              <FormControl>
+                <FormLabel htmlFor="chessUsername">Lichess Username</FormLabel>
                 <TextField
                   id="chessUsername"
                   type="text"
@@ -281,61 +269,54 @@ export default function BetForm(props) {
                 />
               </FormControl>
             
+              {/* Opponent Chess Username */}
               <FormControl>
-                <FormLabel htmlFor="oppChessUsername">
-                  Opponent Lichess Username
-                </FormLabel>
+                <FormLabel htmlFor="oppChessUsername">Opponent Lichess Username</FormLabel>
                 <TextField
                   id="oppChessUsername"
                   type="text"
                   value={opp_chessUsername}
                   onChange={(e) => setOppChessUsername(e.target.value)}
-                  placeholder={`Enter your opponents username`}
+                  placeholder={`Enter your opponent's username`}
                   required
                   fullWidth
                   variant="outlined"
                 />
               </FormControl>
-
-            
-           
-            {/* Stake */}
-            <FormControl>
-              <FormLabel htmlFor="stake">Stake</FormLabel>
-              <TextField
-                id="stake"
-                type="number"
-                value={stake}
-                onChange={(e) => setStake(e.target.value)}
-                placeholder="Enter the stake amount"
-                required
+    
+              {/* Stake */}
+              <FormControl>
+                <FormLabel htmlFor="stake">Stake</FormLabel>
+                <TextField
+                  id="stake"
+                  type="number"
+                  value={stake}
+                  onChange={(e) => setStake(e.target.value)}
+                  placeholder="Enter the stake amount"
+                  required
+                  fullWidth
+                  variant="outlined"
+                  inputProps={{
+                    min: 0,
+                    step: 0.01,
+                  }}
+                />
+              </FormControl>
+    
+              {error && <Typography sx={{ color: 'red' }}>{error}</Typography>}
+    
+              {/* Submit Button */}
+              <Button
+                type="submit"
                 fullWidth
-                variant="outlined"
-                inputProps={{
-                  min: 0,
-                  step: 0.01,
-                }}
-              />
-            </FormControl>
-
-            
-            {error && <Typography sx={{ color: 'red' }}>{error}</Typography>}
-
-            {/* Success Message
-            {successMessage && <Typography sx={{ color: 'green' }}>{successMessage}</Typography>} */}
-
-            {/* Submit Button */}
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-            >
-              Place Bet
-            </Button>
-          </Box>
-        </Card>
-      </BetContainer>
-    </AppTheme>
-  );
-
+                variant="contained"
+              >
+                Place Bet
+              </Button>
+            </Box>
+          </Card>
+        </BetContainer>
+      </AppTheme>
+    );
+    
 }
