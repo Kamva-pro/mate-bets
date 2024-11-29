a04332b65eba39b611290c9c6105920151175ea5
Kamva-pro <kamvelihlefatman@gmail.com>
2024-11-29 12:43:40 +0200

    fixed bg on betform

--- frontend/src/components/BetForm.jsx
+++ frontend/src/components/BetForm.jsx
@@ -27,14 +27,11 @@ const Card = styled(MuiCard)(({ theme }) => ({
   flexDirection: 'column',
   alignSelf: 'center',
   width: '100%',
-  padding: theme.spacing(3),
-  gap: theme.spacing(1.5),
+  padding: theme.spacing(4),
+  gap: theme.spacing(2),
   margin: 'auto',
-  maxWidth: '100%',
   [theme.breakpoints.up('sm')]: {
     maxWidth: '450px',
-    padding: theme.spacing(4),
-    gap: theme.spacing(2),
   },
   boxShadow:
     'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
@@ -44,44 +41,28 @@ const Card = styled(MuiCard)(({ theme }) => ({
   }),
 }));
 
+
 const BetContainer = styled(Stack)(({ theme }) => ({
-  marginTop: '60px',
-  minHeight: '100vh',
-  width: '100%',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
+  height: 'calc((1 - var(--template-frame-height, 0)) * 100dvh)',
+  minHeight: '100%',
   padding: theme.spacing(2),
   [theme.breakpoints.up('sm')]: {
     padding: theme.spacing(4),
   },
-  backgroundImage: `
-    linear-gradient(
-      rgba(18, 18, 18, 0.8), 
-      rgba(18, 18, 18, 0.8)
-    ), 
-    radial-gradient(
-      ellipse at center, 
-      hsl(210, 100%, 97%), 
-      hsl(0, 0%, 100%)
-    )`,
-  backgroundBlendMode: 'overlay', 
-  backgroundSize: 'cover',
-  backgroundAttachment: 'fixed',
-  ...theme.applyStyles('dark', {
-    backgroundColor: '#121212',
-    backgroundImage: `
-      linear-gradient(
-        rgba(18, 18, 18, 0.8), 
-        rgba(18, 18, 18, 0.8)
-      ), 
-      radial-gradient(
-        ellipse at center, 
-        hsla(210, 100%, 16%, 0.5), 
-        hsl(220, 30%, 5%)
-      )`,
-    backgroundBlendMode: 'overlay',
-  }),
+  '&::before': {
+    content: '""',
+    display: 'block',
+    position: 'absolute',
+    zIndex: -1,
+    inset: 0,
+    backgroundImage:
+      'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
+    backgroundRepeat: 'no-repeat',
+    ...theme.applyStyles('dark', {
+      backgroundImage:
+        'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))',
+    }),
+  },
 }));
 
 export default function BetForm(props) {
