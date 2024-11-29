c6af6ef912df5b723dd3d23eca9945d0cfa6e21f
Kamva-pro <kamvelihlefatman@gmail.com>
2024-11-29 12:39:50 +0200

    fixed sign in background color

--- frontend/src/components/sign-in/SignIn.jsx
+++ frontend/src/components/sign-in/SignIn.jsx
@@ -28,14 +28,11 @@ const Card = styled(MuiCard)(({ theme }) => ({
   flexDirection: 'column',
   alignSelf: 'center',
   width: '100%',
-  padding: theme.spacing(3),
-  gap: theme.spacing(1.5),
+  padding: theme.spacing(4),
+  gap: theme.spacing(2),
   margin: 'auto',
-  maxWidth: '100%',
   [theme.breakpoints.up('sm')]: {
     maxWidth: '450px',
-    padding: theme.spacing(4),
-    gap: theme.spacing(2),
   },
   boxShadow:
     'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
@@ -46,24 +43,28 @@ const Card = styled(MuiCard)(({ theme }) => ({
 }));
 
 const SignInContainer = styled(Stack)(({ theme }) => ({
-  minHeight: '100vh',
-  width: '100%',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
+  height: 'calc((1 - var(--template-frame-height, 0)) * 100dvh)',
+  minHeight: '100%',
   padding: theme.spacing(2),
   [theme.breakpoints.up('sm')]: {
     padding: theme.spacing(4),
   },
-  backgroundImage:
-    'radial-gradient(ellipse at center, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
-  backgroundSize: 'cover',
-  backgroundAttachment: 'fixed',
-  ...theme.applyStyles('dark', {
+  '&::before': {
+    content: '""',
+    display: 'block',
+    position: 'absolute',
+    zIndex: -1,
+    inset: 0,
     backgroundImage:
-      'radial-gradient(ellipse at center, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))',
-  }),
+      'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
+    backgroundRepeat: 'no-repeat',
+    ...theme.applyStyles('dark', {
+      backgroundImage:
+        'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))',
+    }),
+  },
 }));
+
 
 export default function SignIn(props) {
   const [emailError, setEmailError] = React.useState(false);
@@ -165,8 +166,9 @@ const handleSubmit = async (event) => {
   return (
     
     <AppTheme {...props}>
-      <SignInContainer>
-      {alertMessage && (
+      <CssBaseline enableColorScheme />
+      <SignInContainer direction="column" justifyContent="space-between">
+        <ColorModeSelect sx={{ position: 'fixed', top: '1rem', right: '1rem' }} />      {alertMessage && (
         <Alert variant="outlined" severity={alertSeverity} style={{ marginBottom: '20px' }}>
           {alertMessage}
         </Alert>
