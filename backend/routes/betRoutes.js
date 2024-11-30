9e38ae6890acab51ee9db3210c9d9b3048875d96
Kamva-pro <kamvelihlefatman@gmail.com>
2024-11-30 19:07:48 +0200

    bet routes

---
+++ backend/routes/betRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { placeBetController } = require('../controllers/betController');
+
+// Route to place a bet
+router.post('/place-bet', placeBetController);
+
+module.exports = router;
