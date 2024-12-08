b0176309d19596c278fad9c8c0f493ab37373537
Kamva-pro <kamvelihlefatman@gmail.com>
2024-12-09 01:47:10 +0200

    exported user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,31 @@
 const supabase = require('../../supabase-client');
 
-const user = (req, res) => {
+const user = async (req, res) => {
     const userId = localStorage.getItem("userId");
+
+    const { data: userData, error: fetchError } = await supabase
+            .from('users')
+            .select('*')
+            .eq('id', userId)
+            .single();
+
+    if (fetchError || !userData) {
+        return res.status(500).json({ message: "Error fetching user data from the database." });
+    }
+
+
+    return res.status(200).json({
+        message: "User data successfully retrieved",
+        token: token,
+        user: {
+            uid: userRecord.uid,
+            email: userRecord.email,
+            displayName: userRecord.displayName,
+            databaseInfo: userData, 
+        },
+
+    });
+    
 }
+
+module.exports = {user};
