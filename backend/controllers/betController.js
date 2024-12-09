1605629624a69cd961e00b5b881a681aadbecd52
Kamva-pro <kamvelihlefatman@gmail.com>
2024-12-09 09:03:04 +0200

    updated bet controller

--- backend/controllers/betController.js
+++ backend/controllers/betController.js
@@ -1,4 +1,3 @@
-// backend/controllers/betController.js
 const supabase = require('../../supabase-client');
 
 const placeBet = async (req, res) => {
@@ -21,6 +20,7 @@ const placeBet = async (req, res) => {
             .select('id', 'balance')
             .eq('email', opponentEmail)
             .single();
+        
 
         if (userError || !userData) {
             return res.status(400).json({ message: 'User not found' });
@@ -28,6 +28,12 @@ const placeBet = async (req, res) => {
 
         if (userData.balance < stake) {
             return res.status(400).json({ message: 'Insufficient funds to place the bet' });
+        }
+
+        else if(opponentData.balance < stake)
+        {
+            return res.status(401).json({ message: 'Opponent has Insufficient funds to place the bet' });
+
         }
 
         const { data: betData, error: betError } = await supabase
@@ -53,6 +59,7 @@ const placeBet = async (req, res) => {
         }
 
         const newBalance = userData.balance - stake;
+        const newOppBalance = opponentData.balance - stake;
         const { error: updateError } = await supabase
             .from('users')
             .update({ balance: newBalance })
@@ -62,7 +69,17 @@ const placeBet = async (req, res) => {
             return res.status(500).json({ message: 'Error updating balance' });
         }
 
-        res.status(200).json({ message: 'Bet placed successfully' });
+        const {error: oppUpdateError} = await supabase
+            .from('users')
+            .select({balance: newOppBalance})
+            .eq('id', opponentData.id);
+        
+        if (oppUpdateError)
+        {
+            return res.status(501).json({message: `Error updating opponent balance`});
+        }
+
+        res.status(201).json({ message: 'Bet placed successfully' });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'An error occurred while placing the bet' });
