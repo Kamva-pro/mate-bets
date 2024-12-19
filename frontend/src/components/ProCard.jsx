18634199257081c72c07b63bc2a8740202293e64
Kamva-pro <kamvelihlefatman@gmail.com>
2024-12-19 11:18:53 +0200

    removed betselect

--- frontend/src/components/ProCard.jsx
+++ frontend/src/components/ProCard.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import "../css/ProGames.css";
+import { useNavigate } from "react-router-dom";
 
 const ProCard = ({ playerOne, playerOneImg, playerTwo, playerTwoImg }) => {
+
+
     return (
-        <div className="progames-card">
+        <div onClick={handleClick(playerOne, playerTwo)} className="progames-card">
             <div className="player-info">
                 <img className="profile-thumbnail" src={playerOneImg} alt={playerOne} />
+                <div className="picks"></div>
                 <p className="playername">{playerOne}</p>
             </div>
             <h3>VS</h3>
             <div className="player-info">
                 <img className="profile-thumbnail" src={playerTwoImg} alt={playerTwo} />
+                <div className="picks"></div>
+
                 <p className="playername">{playerTwo}</p>
             </div>
         </div>
