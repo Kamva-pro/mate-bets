57242fc34e63af1a2f24fdbdd767a0fe5dcca8c7
Kamva-pro <kamvelihlefatman@gmail.com>
2025-02-14 08:32:20 +0200

    fetched live bets from database

--- backend/controllers/fetchBets.js
+++ backend/controllers/fetchBets.js
@@ -1,9 +1,41 @@
+const { messaging } = require('firebase-admin');
 const supabase = require('../../supabase-client');
 
 const fetchBets = async (req, res) => {
   const { userId } = req.query;
 
   try {
+    const {data: liveBets, error: liveBetsError} = await supabase
+      .from('live-bets')
+      .select("*")
+      .eq('id', userId)
+
+    if (liveBetsError)
+    {
+      return res.status(503).json({
+        success: false,
+        error: liveBetsError.message,
+        message: "Failed to fetch live bets"
+      })
+    }
+
+    else if(!liveBets)
+    {
+      return res.status(404).json({
+        success: false,
+        message: "You have no live bets"
+      })
+    }
+
+    else if(liveBets)
+    {
+      return res.status(200).json({
+        success: true,
+        message: "bets retrieved successfully",
+        data: liveBets
+      })
+    }
+
     const { data: userBets, error: userBetsError } = await supabase
       .from('p2p_bets')
       .select('*')
@@ -23,6 +55,7 @@ const fetchBets = async (req, res) => {
       message: 'Bets retrieved successfully.',
       data: userBets,
     });
+    
   } catch (err) {
     console.error('Unexpected error:', err.message);
     return res.status(500).json({
